060b29b4e39a99e3a2e17e80fbae0af5569435a6
andersonpioner <andersonpioner@gmail.com>
2016-01-30 01:48:08 +0000

    Adicionado mensagens de erros, show layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,13 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-      
+    if @article.save
+      flash[:notice] = "Article was succesfuly created" #it only works if you add a code on the view page, in this case aplication view, which is a file that is shown on all the app pages
+      redirect_to article_path(@article) #article_path depends on the path on the rake routes command
+    else
+      render 'new'
+    end
+    
   end
 
   def edit
@@ -16,7 +20,8 @@ class ArticlesController < ApplicationController
   end
   
   def show
-      
+    @article = Article.find(params[:id])
+    
   end
   
   def update
